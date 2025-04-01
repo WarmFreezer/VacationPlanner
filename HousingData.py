1567cb8a367b8d9098260a5aec06122c89430253
LunaDaSpooky <LunaDaSpooky@outlook.com>
2025-04-01 14:59:27 -0400

    Maybe this works. Changed name of scrape class.

--- HousingData.py
+++ HousingData.py
@@ -1,9 +1,4 @@
-class HousingData: #Objects of this type store the data for a housing object
-    percentBudget = 0 #Stores the percentage of the budget allocated to housing
-    
-    def __init__(self, housing, housingCost):
-        self.housing = housing #Stores the name and the address of the housing option
-        self.housingCost = housingCost #Stores the cost of the housing
+class HousingData(object):
+    """description of class"""
 
-    def CalcPercentBudget(budget):
-        percentBudget = housingCost/budget
+
