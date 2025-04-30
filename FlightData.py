1986c46db5d984d1cb49891405a5e2487697caa3
LunaDaSpook <LunaDaSpooky@outlook.com>
2025-04-30 14:52:22 -0400

    Complete FlightScraper and FlightData classes.

--- FlightData.py
+++ FlightData.py
@@ -4,19 +4,19 @@ class FlightData: #Objects of this type contain flight information for other cla
     d_time: str
     price: float
 
-    def __init__(self, airline = "", destination = "", d_time = "", r_time = "", price = 0.0):
+    def __init__(self, airline = "", d_time = "", r_time = "", price = 0.0):
         self.airline = airline #Stores the airline of the flight
-        self.destination = destination #Stores the destination of the flight
         self.d_time = d_time #Stores the departure date/time
         self.r_time = r_time
         self.price = price #Stores the price of the flight
 
+    #This is just to display the results. Appends each entry into an array and returns the array.
     def ToString(self):
         string = []
         string.append(self.airline)
         string.append(self.d_time)
         string.append(self.r_time)
-        string.append(str(self.price))
+        string.append("$" + str(self.price))
         return string
 
 
--- FlightScraper.py
+++ FlightScraper.py
@@ -1,3 +1,6 @@
+#By: James Bandy
+#Gets the cheapest flight option for our program
+
 #This program searches for available flights off of Google Flights using a Chrome Web Browser
 from re import L
 from FlightData import FlightData
@@ -7,6 +10,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import bs4
 import time
 import os
@@ -14,51 +18,65 @@ import os
 
 
 class FlightScraper:
+    #These are the attributes used in the FlightData class
     airline = ""
-    dest = ""
     dt = ""
     rt = ""
     price = 0.0
     
+    #This initializes a search and fills out the attributes
     def __init__(self, url: str):
-        #Setup headless Chrome
+        #This sets up a chrome browser to search for the information using SkipLagged.com
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        #chrome_options.add_argument("--headless") -- Headless mode is meant to hide the browser we open but unfortunately headless mode causes problems with javascript in web applications
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
 
+        #This launches a chromedriver using a url that is entered as input. The url is determined by the inputs which leads to a version of the website with the information needed
         base_dir = os.path.dirname(os.path.abspath(__file__))
         chromedriver_path = os.path.join(base_dir, "chromedriver-win64", "chromedriver.exe")
         service = Service(executable_path=chromedriver_path)
         driver = webdriver.Chrome(options=chrome_options)
-        #print(url)
-        driver.get(url) 
+        driver.get(url) #This opens the webbrowser
 
+        #This scrolls to the bottom to ensure the whole page is loaded. Our results are at the top so mostly not needed but it doesn't hurt to make sure
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)
-
-        print(driver.current_url)
-
-        elem = driver.find_element(By.CLASS_NAME, 'infinite-trip-list')
-        html = elem.get_attribute('outerHTML')
+        time.sleep(5) #The program will wait for a few seconds to ensure everything loads
+        
+        #This parses most of the html document into a string.
+        html = driver.find_element(By.XPATH, '/html/body/section').get_attribute("outerHTML")
         soup = bs4.BeautifulSoup(html, 'html.parser')
 
-        print(soup.prettify())
-
+        #At this point, the information we need is in the parse html document so we close the browser
         driver.quit()
 
-        if "We couldn't display your results" in soup.text:
-            print("No flight results found. Page returned an error message.")
-        else:
-            flight = soup.select_one('')
-            if flight:
-                self.airline = str(flight.find('span').text_strip())
-                path = flight.find('div', class_='span9 trip-path')
-                self.dt = str(path.find('div', class_='trip-path-point trip-path-point-first').find('div', class_='trip-path-point-time ').text_strip())
-                self.rt = str(path.find('div', class_='trip-path-point trip-path-point-last').find('div', class_='trip-path-point-time ').text_strip())
-                self.price = float(flight.find('div', class_='span2 trip-cost').find('p').find('span').text_strip())
-            else:
-                print("There are no available flights")
+        #print(soup.prettify()) -- This was used for debugging purposes
+
+        #This gets the airline and assigns it to the airline variable. It uses the top entry since by default the page sorts by cheapest at the top.
+        #Since we're a budget planner its best to get the cheapest option for our planner.
+        fl = soup.find('span', class_="airlines airlines-lg hide-small")
+        if fl:
+            self.airline = fl.text.strip()
+            #print("Airline is: ", self.airline)
+
+        #This gets the first price on the page and formats it to a float. The reason its not directly accessed like the others is because
+        #it has a malformed identifier and as such couldn't be directly accessed.
+        for span in soup.find_all('span'):
+            text = span.text.strip()
+            if "$" in text:
+                print("Price found:", text)
+                #self.price = float(text[1:])
+                break
+        
+        #This gets all the times on the page and parses the first two (the top results)
+        times = soup.find_all('div', class_="trip-path-point-time")
+        if len(times) >= 2:
+            self.dt = times[0].text.strip()
+            #print("Depart time: ", self.dt)
+            self.rt = times[1].text.strip()
+            #print("Return time: ", self.rt)
+        #else:
+            #print("There are no times")
 
     def getFlight(self) -> FlightData:
         return FlightData(self.airline, self.dt, self.rt, self.price)
